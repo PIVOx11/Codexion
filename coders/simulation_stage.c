0d79584d56aaeba4ce83ffc7460a378d4a36cdd7
Brahim Lidrissi <blidriss@c1r8p2.1337.ma>
2026-06-07 15:19:34 +0100

    add the option of one coder

--- coders/simulation_stage.c
+++ coders/simulation_stage.c
@@ -6,17 +6,39 @@
 /*   By: blidriss <blidriss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 09:15:28 by blidriss          #+#    #+#             */
-/*   Updated: 2026/06/07 11:55:09 by blidriss         ###   ########.fr       */
+/*   Updated: 2026/06/07 15:18:09 by blidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+static void    *semulate_one(void *c)
+{
+    t_coder     *coder;
+    
+    coder = (t_coder *)c;
+    wait_all(coder);
+    set_time(&coder->coder_mutex, &coder->last_compile_start, coder->data->start_semulation);
+    pthread_mutex_lock(&coder->left_dongle->dongle_mutex);
+    safe_print("hase take a dongle", coder);
+    while (!get_bool(&coder->data->stop, &coder->data->is_semulation_over))
+    {
+        usleep(500);
+    }
+    pthread_mutex_unlock(&coder->left_dongle->dongle_mutex);
+    return NULL;
+}
 static int    creat_join_coders(t_data *data, int  join)
 {
     int     i;
 
     i = -1;
+    if (data->coder_count == 1 && !join)
+    {
+        if(pthread_create(&data->coders[0].thread_id, NULL, semulate_one, &data->coders[0]))
+            return FALSE;
+        return TRUE;
+    }
     if (!join)
     {
         while (++i < data->coder_count)
@@ -56,6 +78,8 @@ void *semulation(void *co)
     }
     return NULL;
 }
+
+
 
 int start_semulation(t_data *data)
 {
